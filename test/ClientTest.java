5e0ac9ee3b994704e00d82b38607d7cc9ca33fb2
Aleksei.Tirman <aleksei.tirman@jetbrains.com>
2026-04-22 11:44:07 +0300

    Add test for 204 status

---
+++ test/ClientTest.java
@@ -0,0 +1,43 @@
+import me.stexe.http2.Http2Client;
+import me.stexe.http2.Http2Request;
+import me.stexe.http2.HttpMethod;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ClientTest {
+
+    @Test
+    @Timeout(5)
+    void noContent() throws IOException, URISyntaxException, ExecutionException, InterruptedException {
+        var server = new ProcessBuilder("test-data/.venv/bin/python3", "test-data/no-content.py").start();
+
+        var scanner = new Scanner(server.getInputStream());
+
+        try {
+            var port = Integer.parseInt(scanner.nextLine());
+
+            try (var client = Http2Client.connectH2c("localhost", port)) {
+                var response = client.send(
+                    new Http2Request(HttpMethod.GET, new URI("http://localhost:%d/".formatted(port)).toURL())
+                );
+
+                assertEquals(204, response.status());
+                assertEquals(0, response.body().length);
+                // TODO: Check other headers
+            }
+        } finally {
+            server.destroy();
+
+            String stderr = new String(server.getErrorStream().readAllBytes());
+            System.out.println(stderr);
+        }
+    }
+}
